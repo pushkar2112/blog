b5f11e328d635213576191e7fc98cbc5e5e0cf8a
Pushkar Verma <pushkar7397@gmail.com>
2022-06-10 14:39:44 +0530

    PostCreate App working

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
-import axios from 'axios'; 
+import axios from "axios";
 
-const onSubmit = async (event) => {
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post('http://localhost:4000/posts', {
-        title
+    await axios.post("http://localhost:4000/posts", {
+      title,
     });
 
-    setTitle('');
+    setTitle("");
+  };
 
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
 };
 
-const App = () => {
-    const [title, setTitle] = useState('');
-    return <div>
-        <form method="post" onSubmit={onSubmit}>
-            <div className="form-group">
-                <label>Title</label>
-                <input  type="form" value={title} onChange={e => setTitle(e.target.value)}  className="form-control"/>
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
-    </div>;
-};
- 
-export default App;
+export default PostCreate;
